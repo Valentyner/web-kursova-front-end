5a1713a884aebb7e47948422c81178a94f85dc4a
Valentyner <robertdoms2002@gmail.com>
2023-11-03 11:54:06 +0200

    Version 1.0.0

---
+++ src/Header/MyHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../Header/MyHeader.css";
+
+const MyHeader = () => {
+    return (
+        <div>
+            React
+        </div>
+    );
+};
+
+export default MyHeader;
