756b9ce08e8b1ea7877d81f9069affeaaa597d73
Valentyner <robertdoms2002@gmail.com>
2023-11-15 17:48:30 +0200

    Final_Version_1.0.1

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -8,15 +8,6 @@ const MainPage = () => {
         <>
         <NavBar/>
             <Slider />
-            <div style={{ marginTop: "670px" }}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus facilis quia culpa tempora et perspiciatis suscipit, ullam totam odit repellat! Iste eveniet quidem enim consectetur vitae? Atque sapiente ipsum enim.
-            </div>
-            <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus facilis quia culpa tempora et perspiciatis suscipit, ullam totam odit repellat! Iste eveniet quidem enim consectetur vitae? Atque sapiente ipsum enim.
-            </div>
-            <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus facilis quia culpa tempora et perspiciatis suscipit, ullam totam odit repellat! Iste eveniet quidem enim consectetur vitae? Atque sapiente ipsum enim.
-            </div>
         </>
 
     );
